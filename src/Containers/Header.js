4aaa17f7e8ab947743bda28bc82fbe7d00998812
Alex-Lyu0419 <alexlyu0419@gmail.com>
2022-12-30 16:55:50 +0800

    fix bugs

--- src/Containers/Header.js
+++ src/Containers/Header.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button/index.js';
+import Typography from '@material-ui/core/Typography/index.js';
 
 import axios from '../api.js';
 import { useScoreCard } from '../hooks/useScoreCard.js';
